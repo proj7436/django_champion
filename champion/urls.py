f5d7128227f30278f64a5e7e8736f04233b4e166
ptdat <112596519+Dat240906@users.noreply.github.com>
2023-08-11 14:30:00 +0000

    add urrls

--- champion/urls.py
+++ champion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Main, AdminSite
+from .views import Main, AdminSite, ranking_view
 
 
 urlpatterns = [
+    path('view', ranking_view, name='víe'),
     path('champion', Main.as_view(), name='main'),
     path('admin06', AdminSite.as_view(), name='admin')
 ]
--- champion/views.py
+++ champion/views.py
@@ -4,8 +4,46 @@ from .models import Champion
 # Create your views here.
 
 
+
+def sort_key(champion):
+    return (-champion.point, -champion.h_s)
+
+def ranking_view(request):
+    # Lấy tất cả các đối tượng từ database và sắp xếp
+    teams_from_db = Champion.objects.all().order_by('-point', '-h_s')
+    
+    # Đặt rank cho các đội dựa trên thứ tự sắp xếp
+    rank = 1
+    for team in teams_from_db:
+        team.rank = rank
+        rank += 1
+    
+    # Tạo nội dung HTML từ danh sách đã sắp xếp và đã thêm rank
+    table_html = "<table>"
+    for team in teams_from_db:
+        table_html += f"<tr><td>{team.rank}</td><td>{team.name}</td><td>{team.point}</td><td>{team.h_s}</td></tr>"
+    table_html += "</table>"
+    
+    # Trả về HttpResponse chứa nội dung HTML
+    response = HttpResponse(table_html)
+    return response
+
+
 class Main(View):
     def get(self, request):
+
+        # xét xếp hạng dựa trên điểm, và điểm bằng nhau xét tới HS
+        list_object = Champion.objects.all()
+        def sort_key(team):
+            return (-team.point, -team.h_s)
+
+         # Sắp xếp danh sách các đối tượng
+        sorted_teams = sorted(list_object, key=sort_key)
+
+# In danh sách đã sắp xếp
+        for team in sorted_teams:
+            print(f"Name: {team.name}, Point: {team.point}, H/S: {team.h_s}")
+
         return render(request, 'main.html')
     
 
