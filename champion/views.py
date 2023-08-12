fc4c1106ce75b4c7b66e3b93be2a68c0e1a2767a
ptdat <112596519+Dat240906@users.noreply.github.com>
2023-08-12 01:22:45 +0000

    add post

--- champion/views.py
+++ champion/views.py
@@ -27,3 +27,33 @@ class AdminSite(View):
 
         oj = Champion.objects.all()
         return render(request, 'admin.html', {"list":oj})
+    
+
+    def post(self, request):
+
+        input_ = request.POST
+
+
+        team1_name = input_.get('team1_name')
+        team2_name = input_.get('team2_name')
+
+        score1 = input_.get('score1')
+        score2 = input_.get('score2')
+
+        oj1 = Champion.objects.get(name_team = team1_name)
+        oj2 = Champion.objects.get(name_team = team2_name)
+
+        if score1 > score2:
+            oj1.count_win += 1
+            oj2.count_lose += 1
+        elif score1== score2:
+            oj1.count_draw += 1
+            oj2.count_draw += 1
+        else:
+            oj1.count_lose += 1
+            oj2.count_win += 1
+
+        oj1.save()
+        oj2.save()
+
+        return render(request, 'admin.html', {'message':'Cap nhat thanh cong!'})
